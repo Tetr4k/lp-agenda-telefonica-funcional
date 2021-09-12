4e357b21ad7480c7b755ff04d7ae5e723bed1250
Gabriel <g2012ribeiro@gmail.com>
2021-09-11 22:54:36 -0300

    Passos

--- Haskell/Main.hs
+++ Haskell/Main.hs
@@ -7,4 +7,10 @@ type Contato = (Nome, Numero, Endereco, Relacao)
 type Agenda = [Contato]
 main :: IO ()
 main = do
+-- Inserir o Contato "Fulano", 99999999, "Rua A", "UFF";
+-- Inserir o Contato "Ciclano", 88888888, "Rua B", "Cederj";
+-- Inserir o Contato "Beltrano", 88889999, "Rua C", "Infância";
+-- Inserir o Contato "Fulano", 77777777, "Rua D", none;
+-- Remover o Contato "Ciclano";
+-- Listar o conteudo da Agenda;
  putStrLn "Hello World"
