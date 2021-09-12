bb1584ceae880e6b03c0db1d1079a4b10cfc29f2
Gabriel <g2012ribeiro@gmail.com>
2021-09-11 22:47:37 -0300

    Tipos

--- Haskell/Main.hs
+++ Haskell/Main.hs
@@ -1,4 +1,10 @@
 module Main where
+type Nome = [Char]
+type Numero = Int
+type Endereco = [Char]
+type Relacao = [Char]
+type Contato = (Nome, Numero, Endereco, Relacao)
+type Agenda = [Contato]
 main :: IO ()
 main = do
  putStrLn "Hello World"
