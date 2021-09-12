35cdeaa595e17db59e785b8cf1de042d00ca3862
Gabriel <g2012ribeiro@gmail.com>
2021-09-11 22:59:03 -0300

    Listagem do conteudo da agenda

--- Haskell/Principal.hs
+++ Haskell/Principal.hs
@@ -9,10 +9,12 @@ type Agenda = [Contato]
 
 main :: IO ()
 main = do
+ let agenda = []
+
 -- Inserir o Contato "Fulano", 99999999, "Rua A", "UFF";
 -- Inserir o Contato "Ciclano", 88888888, "Rua B", "Cederj";
 -- Inserir o Contato "Beltrano", 88889999, "Rua C", "Infância";
 -- Inserir o Contato "Fulano", 77777777, "Rua D", none;
 -- Remover o Contato "Ciclano";
--- Listar o conteudo da Agenda;
- putStrLn "Hello World"
+
+ putStrLn ("Contatos" ++ listar agenda)
