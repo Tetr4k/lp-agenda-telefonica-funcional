ec04fe941c7d375576bf177ce2202f05cd7b48c9
Gabriel <g2012ribeiro@gmail.com>
2021-09-11 22:57:27 -0300

    Mudança na abordagem

--- Haskell/Principal.hs
+++ Haskell/Principal.hs
@@ -1,10 +1,12 @@
-module Main where
+module Principal where
+
 type Nome = [Char]
 type Numero = Int
 type Endereco = [Char]
 type Relacao = [Char]
 type Contato = (Nome, Numero, Endereco, Relacao)
 type Agenda = [Contato]
+
 main :: IO ()
 main = do
 -- Inserir o Contato "Fulano", 99999999, "Rua A", "UFF";
